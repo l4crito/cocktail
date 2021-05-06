9b035454f4b1796ad817ef793b37590b04327483
l4crito <l4crito@gmail.com>
2021-05-06 18:59:16 -0400

    cache

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,15 @@ import { topToBottomAnimation } from 'src/shared/animations';
 import { UpdateService } from 'src/shared/services/UpdateService';
 
 @Component({
-  animations:[topToBottomAnimation],
+  animations: [topToBottomAnimation],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(public updateService: UpdateService) { }
   title = 'cocktail';
+  constructor(public updateService: UpdateService) { }
+  updateApp() {
+    document.location.reload();
+  }
 }
